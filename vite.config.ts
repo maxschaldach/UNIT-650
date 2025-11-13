c4516bb478a3d24249360f3aef6b855e9f860a78
Max Schaldach <max@schaldach.com>
2025-11-12 16:15:40 -0800

    finalize vite config for gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/", // 👈 custom domain, served from root
+  base: "/", // 👈 because you're using a custom domain (unit650.vc)
   server: {
     host: "::",
     port: 8080,
